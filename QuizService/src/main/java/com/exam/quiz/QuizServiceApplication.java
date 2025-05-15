296f24718515e855a6d2b74c42beb87dca4386ca
IamVk009 <imvk009@gmail.com>
2025-05-15 19:13:55 +0530

    Exam Portal : Quiz Service - Configure FeignClient for Communication with Question Microservice

--- QuizService/src/main/java/com/exam/quiz/QuizServiceApplication.java
+++ QuizService/src/main/java/com/exam/quiz/QuizServiceApplication.java
@@ -2,8 +2,19 @@ package com.exam.quiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ *
+ * @EnableFeignClients
+ *
+ * Spring annotation that enables the detection and registration of Feign client interfaces in your application.
+ * Scan for interfaces annotated with @FeignClient in the specified package(s).
+ * Automatically generate runtime implementations of those interfaces.
+ * Register them as Spring beans, so you can inject and use them like regular services.
+ */
 @SpringBootApplication
+@EnableFeignClients
 public class QuizServiceApplication {
 
 	public static void main(String[] args) {
---
+++ QuizService/src/main/java/com/exam/quiz/service/QuestionClient.java
@@ -0,0 +1,15 @@
+package com.exam.quiz.service;
+
+import com.exam.quiz.entity.Question;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(url = "http://localhost:8082", value = "Question Client")
+public interface QuestionClient {
+
+    @GetMapping("/api/v1/questions/quiz/{quizId}")
+    List<Question> getQuestionOfQuiz(@PathVariable Long quizId);
+}
