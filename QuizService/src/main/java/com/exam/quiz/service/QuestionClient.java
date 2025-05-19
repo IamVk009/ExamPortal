c099bece3e174c293ad4db23e01adc521bc869db
IamVk009 <imvk009@gmail.com>
2025-05-19 10:37:29 +0530

    Exam Portal : Quiz Service - Load Balancing Configuration

--- QuizService/src/main/java/com/exam/quiz/service/QuestionClient.java
+++ QuizService/src/main/java/com/exam/quiz/service/QuestionClient.java
@@ -7,7 +7,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(url = "http://localhost:8082", value = "Client")
+/**
+ * Handles service communication using service names instead of fixed URLs.
+ * Since multiple instances of the same service may run on different URLs,
+ * using a service name allows a load balancer to distribute traffic
+ * dynamically across those instances.
+ *
+ * This enables load balancing and improves fault tolerance and scalability.
+ * Instead of hardcoding service URLs, we refer to them by name (e.g., via Eureka),
+ * allowing the load balancer to choose an available instance.
+ *
+ */
+//@FeignClient(url = "http://localhost:8082", value = "Client")
+@FeignClient(name = "QUESTION-SERVICE")
 public interface QuestionClient {
 
     @GetMapping("/api/v1/questions/quiz/{quizId}")
